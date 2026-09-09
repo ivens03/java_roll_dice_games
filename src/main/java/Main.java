12e55fbece7bf6235d5bb2f9323a3fe3d18b2ac4
ivens magno <ivensmagno@gmail.com>
2026-09-08 21:47:41 -0300

    começo

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import configuracoesDisplayerDoJogador.Displayer;
+
+void main() {
+
+    // Cria um objeto da sua nova classe TelaJogo
+    Displayer tela = new Displayer();
+
+    // Manda a tela aparecer
+    tela.setVisible(true);
+
+}
